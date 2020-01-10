51759fd1e28bfe0e843137274cf3340375a70aa5
aereal <aereal@aereal.org>
2020-01-10 16:10:40 +0900

    Docker entrypoint

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# syntax = docker/dockerfile:experimental
+
+FROM golang:1.13 as builder
+ENV APP_DIR /go/src/github.com/aereal/neocron
+ENV GO111MODULE on
+ENV CGO_ENABLED 0
+WORKDIR ${APP_DIR}
+COPY go.mod go.sum ./
+RUN --mount=type=cache,target=/go/src/github.com/aereal/neocron/pkg/cache \
+  go get -v all
+COPY ./sleeper/sleeper.go ./sleeper/sleeper.go
+COPY ./cmd/sleeper/main.go ./cmd/sleeper/main.go
+RUN go build -o sleeper ./cmd/sleeper
+
+FROM gcr.io/distroless/static
+ENV APP_DIR /go/src/github.com/aereal/neocron
+COPY --from=builder ${APP_DIR}/sleeper .
+
+ENTRYPOINT [ "./sleeper" ]
---
+++ cmd/sleeper/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aereal/neocron/sleeper"
+)
+
+func main() {
+	if err := run(os.Args); err != nil {
+		log.Printf("! %s", err)
+		os.Exit(1)
+	}
+}
+
+func run(argv []string) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wait, err := parseArgv(argv)
+	if err == flag.ErrHelp {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	if time.Duration(wait).Seconds() == 0 {
+		return fmt.Errorf("wait cannot be 0")
+	}
+
+	if err := sleeper.Run(ctx, wait); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseArgv(argv []string) (wait sleeper.Duration, err error) {
+	flgs := flag.NewFlagSet(argv[0], flag.ContinueOnError)
+	flgs.Var(&wait, "wait", "wait duration")
+
+	err = flgs.Parse(argv[1:])
+	if err != nil {
+		return wait, err
+	}
+
+	return wait, nil
+}
